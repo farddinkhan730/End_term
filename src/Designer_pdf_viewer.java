1d1152cdb50c5aba1f79cf3407a6367db6cb7f0b
farddinkhan730 <farddin.khan_cs19@gla.ac.in>
2020-11-11 20:01:12 +0530

    Designer PDF Viewer solution

---
+++ src/Designer_pdf_viewer.java
@@ -0,0 +1,71 @@
+//public class Designer_pdf_viewer {
+//}
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Solution45132 {
+
+    // Complete the designerPdfViewer function below.
+    static int designerPdfViewer(int[] h, String word) {
+        int count=0;
+        boolean z=false ,y=false;
+        int max=0;
+        for(int i=0;i<word.length();i++){
+            int j=0;
+            if((word.charAt(i)>='a' || word.charAt(i)>='A') && (word.charAt(i)<='Z' || word.charAt(i)<='z')){
+                count++;
+                j=h[i];
+            }
+            if(word.charAt(i)=='z'||word.charAt(i)=='Z'){
+                z=true;
+            }
+            if(word.charAt(i)=='y' || word.charAt(i)=='Y'){
+                y=true;
+            }
+            if(max<j){
+                max=j;
+            }
+        }
+        Arrays.sort(h);
+        if(z){
+            return h[h.length-1]*count;
+        }
+        if(y){
+            return h[h.length-1]*count;
+        }
+
+        return count*max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[] h = new int[26];
+
+        String[] hItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 26; i++) {
+            int hItem = Integer.parseInt(hItems[i]);
+            h[i] = hItem;
+        }
+
+        String word = scanner.nextLine();
+
+        int result = designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
