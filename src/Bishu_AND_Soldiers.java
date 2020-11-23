c5398eb6eab6919c434577d04b5b14f59d311570
farddinkhan730 <farddin.khan_cs19@gla.ac.in>
2020-11-23 21:51:55 +0530

    bishu and soldiers

--- src/Bishu_AND_Soldiers.java
+++ src/Bishu_AND_Soldiers.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -36,5 +37,61 @@ class su{
             sum+=i;
         }
         System.out.println(sum);
+    }
+}
+
+class TestClass42 {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            int l = a.length - 1;
+            int s=0;
+            for (int j = 0; j < k; j++) {
+
+                for (int k1 = l; k1>= 0; k1++) {
+                    int s1=k1;
+                    int temp=a[k1];
+                    while(n-->0) {
+                        a[s1] = a[s1 - 1];
+                        s1--;
+                    }
+                    a[s]=temp;
+                    for (int i = 0; i < a.length; i++) {
+                        System.out.println(a[i]);
+                    }
+                }
+
+            }
+            for (int i = 0; i < a.length; i++) {
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
+class arrayli {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int a[] = new int[n];
+            ArrayList arr = new ArrayList();
+            for (int i = 0; i < n; i++) {
+                arr.add(sc.nextInt());
+            }
+            for (int i = 0; i < k; i++) {
+                arr.add(0,arr.get(n-1));
+                arr.remove(n);
+            }
+            System.out.println(arr);
+        }
     }
 }
